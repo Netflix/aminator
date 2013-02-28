7787bfa6fbfea3d93a2e2609f723395d818a5351
Michael Tripoli <mtripoli@netflix.com>
2013-02-27 19:51:22 -0800

    hardcode the values in ec2_obj_states rather than derive from botocore data. The structure of the botocore ec2 data can change and it has between versions 0.5.3 and 0.7.0.

--- aminator/clouds/ec2/data.py
+++ aminator/clouds/ec2/data.py
@@ -18,41 +18,13 @@
 #
 #
 
-import logging
-
-import botocore.session
-
-log = logging.getLogger(__name__)
-
-_ec2_operations = botocore.session.Session().get_service_data('ec2')['operations']
-_ec2_data = dict((operation['name'], operation) for operation in _ec2_operations)
-
-
-def _find_state_enum(dictionary, path="", search_key='State'):
-    ret = None
-    if isinstance(dictionary, dict):
-        if search_key in dictionary:
-            if search_key == 'enum':
-                log.debug(path, str(dictionary[search_key]))
-                return dictionary[search_key]
-            else:
-                return _find_state_enum(dictionary[search_key], "%s['%s']" % (path, search_key), 'enum')
-        else:
-            for key in dictionary:
-                ret = _find_state_enum(dictionary[key], "%s['%s']" % (path, key), search_key)
-                if ret is not None:
-                    break
-            return ret
-
-
-def ec2_op_states(op):
-    return _find_state_enum(_ec2_data[op])
-
-
-ec2_obj_states = {'Image': ec2_op_states('DescribeImages'),
-                  'Volume': ec2_op_states('DescribeVolumes'),
-                  'Snapshot': ec2_op_states('DescribeSnapshots'),
-                  'Instance': ec2_op_states('DescribeInstances')}
-
-ec2_obj_states['Image'].append('pending')
-ec2_obj_states['Snapshot'].append('100%')
+ec2_obj_states = {
+    'Image': ['available', 'deregistered', 'pending'],
+    'Instance': ['pending',
+                 'running',
+                 'shutting-down',
+                 'terminated',
+                 'stopping',
+                 'stopped'],
+    'Snapshot': ['pending', 'completed', 'error', '100%'],
+    'Volume': ['creating', 'available', 'in-use', 'deleting', 'error']}
