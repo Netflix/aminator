80663720b178d461426fc685958db0d4bb40e74d
Michael Tripoli <mtripoli@netflix.com>
2013-03-02 15:41:30 -0800

    config.Config class plus package level config object initialized with project level defaults and overridden settings.

--- aminator/config.py
+++ aminator/config.py
@@ -17,15 +17,59 @@
 #     limitations under the License.
 #
 #
-import yaml
 
-defaults = """
-logdir: /var/log
-lockdir: /var/lock
-log_url_template: http://%host%:%port%/%uri%/%file%
-minor_device_letters: 'fghijklmnop'
-major_device_letters: 'qrstuvwxyz'
-bind_mounts: [/dev, /proc, /sys]
-device_prefixes: [sd, xvd]
+import yaml
+import os
+import logging
+
+log = logging.getLogger(__name__)
+
+
+__doc__ = """
+# aminator default configuration settings
+# This document is processed by yaml and formatted per
+# http://pyyaml.org/wiki/PyYAMLDocumentation#Documents
+
+config_file: /etc/aminator    # defaults can be overridden in this yaml formatted file.
+lockdir: /var/lock            # lock file directory
+
+device_prefixes: [sd, xvd]             # possible OS device prefixes
+minor_device_letters: 'fghijklmnop'    # devices used for non-partitioned volumes
+major_device_letters: 'qrstuvwxyz'     # devices used for partitioned volumes
+
+volume_root: /aminator/volumes      # volumes mounted onto subdirs of this director.
+bind_mounts: [/dev, /proc, /sys]    # each of these are bind mounted to it's counterpart
+                                    # under the root of the mounted volume.
+
+logdir: /var/log              # destination for debug log files
+
+# elements contained the URL of the 'Detailed logs' console message
+log_url_prefix: "http://localhost:80/"
 """
 
+
+class Config(object):
+    def __init__(self):
+        file_config = {}
+        default_cfg = yaml.load(__doc__)
+        self._set_attrs(**default_cfg)
+        if os.path.exists(self.config_file):
+            with open(self.config_file) as cf:
+                try:
+                    file_config = yaml.load(cf)
+                except yaml.MarkedYAMLError, e:
+                    log.debug("{0}: {1}: line{2}, column{3}".format(e,
+                                                                    e.message,
+                                                                    e.problem_mark.line,
+                                                                    e.problem_mark.column))
+        if len(file_config) > 0:
+            self._set_attrs(**file_config)
+
+    def _set_attrs(self, **kwargs):
+        for i in kwargs:
+            value = kwargs[i]
+            if isinstance(value, list):
+                value = tuple(value)
+            setattr(self, i, value)
+
+config = Config()
