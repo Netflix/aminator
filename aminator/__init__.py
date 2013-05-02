79624e9b806c1532376ed5cf3e931cc677dc9da4
Bob T Builder <builds@netflix.com>
2013-05-02 13:20:59 +0000

    prepare for release 1.2.38-dev

--- aminator/__init__.py
+++ aminator/__init__.py
@@ -35,7 +35,7 @@ except ImportError:
             def emit(self, record):
                 pass
 
-__version__ = '1.2.29dev'
+__version__ = '1.2.38dev'
 __versioninfo__ = __version__.split('.')
 __all__ = ()
 
