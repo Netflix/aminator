0003fb58e5408bc1ceb8de1cca7a1483028a1c30
CloudBees DEV@Cloud <nobody@cloudbees.com>
2014-01-24 14:14:10 -0800

    update release to 2.1.11-dev

--- aminator/__init__.py
+++ aminator/__init__.py
@@ -35,7 +35,7 @@ except ImportError:
             def emit(self, record):
                 pass
 
-__version__ = '2.1.10-dev'
+__version__ = '2.1.11-dev'
 __versioninfo__ = __version__.split('.')
 __all__ = ()
 
