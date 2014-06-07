97671be06798da86b06f0693766589b78d558446
CloudBees DEV@Cloud <nobody@cloudbees.com>
2014-06-06 21:16:11 -0700

    update release to 2.1.48-dev

--- aminator/__init__.py
+++ aminator/__init__.py
@@ -35,7 +35,7 @@ except ImportError:
             def emit(self, record):
                 pass
 
-__version__ = '2.1.47-dev'
+__version__ = '2.1.48-dev'
 __versioninfo__ = __version__.split('.')
 __all__ = ()
 
